6bae116558d5460c916c62b0f995fbe236a45117
Totex <tonyfilpo@azet.sk>
2021-12-05 10:16:50 +0100

    vignette shader

---
+++ vignette.py
@@ -0,0 +1,37 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import loadPrcFileData
+from panda3d.core import Shader
+
+configVars = """
+win-size 1280 720
+show-frame-rate-meter 1
+"""
+
+loadPrcFileData("", configVars)
+
+
+class MyGame(ShowBase):
+    def __init__(self):
+        super().__init__()
+        self.set_background_color(0, 0, 0, 1)
+        # self.wireframeOn()
+        self.cam.setPos(0, -4, 0)
+
+        my_shader = Shader.load(Shader.SL_GLSL,
+                                vertex="shaders/vignette-vert.glsl",
+                                fragment="shaders/vignette-frag.glsl")
+
+        self.plane = self.loader.loadModel("my-models/plane")
+        self.plane.reparentTo(self.render)
+        self.plane.set_shader_input("resolution", (self.win.getXSize(), self.win.getYSize()))
+        self.plane.setShader(my_shader)
+
+        self.accept("aspectRatioChanged", self.win_resize)
+
+    def win_resize(self):
+        # print("resize")
+        self.plane.set_shader_input("resolution", (self.win.getXSize(), self.win.getYSize()))
+
+
+game = MyGame()
+game.run()
